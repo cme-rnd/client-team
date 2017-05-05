cc0a34a948c1b2ff69c9c31cda9cc6d887644734
HieuNT48 <hieunt48@fsoft.com.vn>
2017-05-05 14:09:08 +0700

    Create more dummy data

--- cme-ui/src/app/models/user.ts
+++ cme-ui/src/app/models/user.ts
@@ -16,6 +16,18 @@ export class UserWrapper {
 
 export const CreateUserList: User[] = [
     {firstName: 'Hieu', lastName: 'Nguyen', userName: 'admin', password: 'admin'},
-    {firstName: 'Peter', lastName: 'Tran', userName: 'user', password: 'user'},
+    {firstName: 'Peter 1', lastName: 'Tran', userName: 'user', password: 'user'},
+    {firstName: 'Peter 2', lastName: 'Tran', userName: 'user', password: 'user'},
+    {firstName: 'Peter 3', lastName: 'Tran', userName: 'user', password: 'user'},
+    {firstName: 'Peter 4', lastName: 'Tran', userName: 'user', password: 'user'},
+    {firstName: 'Peter 5', lastName: 'Tran', userName: 'user', password: 'user'},
+    {firstName: 'Peter 6', lastName: 'Tran', userName: 'user', password: 'user'},
+    {firstName: 'Peter 7', lastName: 'Tran', userName: 'user', password: 'user'},
+    {firstName: 'Peter 8', lastName: 'Tran', userName: 'user', password: 'user'},
+    {firstName: 'Peter 9', lastName: 'Tran', userName: 'user', password: 'user'},
+    {firstName: 'Peter 10', lastName: 'Tran', userName: 'user', password: 'user'},
+    {firstName: 'Peter 11', lastName: 'Tran', userName: 'user', password: 'user'},
+    {firstName: 'Peter 12', lastName: 'Tran', userName: 'user', password: 'user'},
+    {firstName: 'Peter 13', lastName: 'Tran', userName: 'user', password: 'user'},
 ];
 
